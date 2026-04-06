977568fe93021a6745a06338fe5fcfa08e2264b4
makotohigashiumc <11221101848@alunos.umc.br>
2026-04-06 00:03:44 -0300

    remoção de comentário de senha no seed.sql

--- qrcode-restaurante/database/seed.sql
+++ qrcode-restaurante/database/seed.sql
@@ -1,7 +1,6 @@
 -- ============================================================
 -- qrcode-restaurante | Dados de exemplo (seed)
 -- Execute APÓS o schema.sql
--- Senha do admin: admin123 (hash bcrypt)
 -- ============================================================
 
 -- Restaurante de exemplo
@@ -10,7 +9,7 @@ VALUES (
     '11111111-1111-1111-1111-111111111111',
     'Sabor & Arte',
     'admin@saborarte.com.br',
-    '$2b$12$LQv3c1yqBWVHxkd0LHAkCOYz6TiGfcLrH4VQKFwjJzMxwRJqSBkHW',  -- senha: admin123
+    '$2b$12$LQv3c1yqBWVHxkd0LHAkCOYz6TiGfcLrH4VQKFwjJzMxwRJqSBkHW',  
     '(11) 99999-0001',
     '08780-000',
     'Mogi das Cruzes',
